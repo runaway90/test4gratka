7dc3847a5e6621d4963f377a83f0ce54b90791b1
dell <vsbezgin@gmail.com>
2026-05-01 22:31:26 +0200

    T1-08. Fix user icon from front(you can see who logged, go to profile and loggout.

--- photogallery-web/src/Controller/HomeController.php
+++ photogallery-web/src/Controller/HomeController.php
@@ -4,35 +4,36 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\LikeRepository;
 use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     public function __construct(
-        private PhotoRepository $photoRepository,
-        private LikeRepository $likeRepository
+        private readonly PhotoRepository $photoRepository,
+        private readonly LikeRepository $likeRepository
     ) {
     }
 
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
-        $photos = $this->photoRepository->findAllWithUsers();
+        $photos = $this->photoRepository->findAll();
         $currentUser = $this->getUser();
         $userLikes = [];
 
-        if ($currentUser) {
-            $this->likeRepository->setUser($currentUser);
+        if ($currentUser instanceof User) {
             foreach ($photos as $photo) {
-                $userLikes[$photo->getId()] = $this->likeRepository->hasUserLikedPhoto($photo);
+                $userLikes[$photo->getId()->toRfc4122()] = $this->likeRepository->findOneByUserAndPhoto($currentUser, $photo) !== null;
             }
         }
 
         return $this->render('home/index.html.twig', [
             'photos' => $photos,
-            'currentUser' => $currentUser,
             'userLikes' => $userLikes,
         ]);
     }
